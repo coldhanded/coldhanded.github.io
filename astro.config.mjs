5406a4ef395c9dbdea60ea746948f90f6b8985e4
Cold Hands <47939989+coldhanded@users.noreply.github.com>
2026-07-09 00:29:27 -0400

    Update site URL for coldhands.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://coldhanded.github.io/",
+  site: "https://coldhands.net",
   vite: {
     plugins: [tailwindcss()]
   }
